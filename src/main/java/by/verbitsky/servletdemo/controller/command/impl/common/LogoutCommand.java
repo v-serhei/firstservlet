ac69d185b221d53cde161e7193d21fc51fe4be8d
Verbitsky Serhey <training.v.sergey@gmail.com>
2020-10-26 02:26:50 +0300

    fix LogoutCommand

--- src/main/java/by/verbitsky/servletdemo/controller/command/impl/common/LogoutCommand.java
+++ src/main/java/by/verbitsky/servletdemo/controller/command/impl/common/LogoutCommand.java
@@ -1,25 +1,24 @@
 package by.verbitsky.servletdemo.controller.command.impl.common;
 
+import by.verbitsky.servletdemo.controller.SessionRequestContent;
+import by.verbitsky.servletdemo.controller.command.AttributeName;
 import by.verbitsky.servletdemo.controller.command.Command;
 import by.verbitsky.servletdemo.controller.command.CommandResult;
-import by.verbitsky.servletdemo.controller.SessionRequestContent;
-import by.verbitsky.servletdemo.entity.User;
-import by.verbitsky.servletdemo.exception.CommandException;
-import by.verbitsky.servletdemo.controller.command.AttributeName;
 import by.verbitsky.servletdemo.controller.command.PagePath;
+import by.verbitsky.servletdemo.entity.User;
 
 public class LogoutCommand implements Command {
 
     @Override
-    public CommandResult execute(SessionRequestContent content) throws CommandException {
+    public CommandResult execute(SessionRequestContent content) {
         User user = (User) content.getSessionAttribute(AttributeName.SESSION_USER);
         CommandResult result;
         if (user != null && user.getLoginStatus()) {
             content.getSession().invalidate();
-        }else {
-            throw new CommandException("LogoutCommand: attribute \"User\" is null\"");
+            result = new CommandResult(PagePath.LOGOUT_PAGE, true);
+        } else {
+            result = new CommandResult(PagePath.MAIN_PAGE, true);
         }
-        result = new CommandResult(PagePath.LOGOUT_PAGE, true);
         return result;
     }
 }
