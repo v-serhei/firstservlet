0f83ddd139065347747c1fe616abfb7ed8fbd1bf
Verbitsky Serhey <training.v.sergey@gmail.com>
2020-11-26 03:45:55 +0300

    add test for FieldDataValidatorTest

--- src/test/java/by/verbitsky/servletdemo/util/FieldDataValidatorTest.java
+++ src/test/java/by/verbitsky/servletdemo/util/FieldDataValidatorTest.java
@@ -1,28 +1,102 @@
 package by.verbitsky.servletdemo.util;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
 
 public class FieldDataValidatorTest {
 
     @Test
-    public void testIsValidUserName() {
+    public void testIsValidUserNamePositive() {
+        String name = "username";
+        Assert.assertTrue(FieldDataValidator.isValidUserName(name));
     }
 
     @Test
-    public void testIsValidUserEmail() {
+    public void testIsValidUserNameNegativeNullParameter() {
+        Assert.assertFalse(FieldDataValidator.isValidUserName(null));
     }
 
     @Test
-    public void testIsValidUserPassword() {
+    public void testIsValidUserNameNegativeWrongParameter() {
+        String name = "username<>";
+        Assert.assertFalse(FieldDataValidator.isValidUserName(name));
     }
 
     @Test
-    public void testValidateRequestParameter() {
+    public void testIsValidUserEmailPositive() {
+        String email = "email@mail.ru";
+        Assert.assertTrue(FieldDataValidator.isValidUserEmail(email));
     }
 
     @Test
-    public void testValidateRequestTextAreaParameter() {
+    public void testIsValidUserEmailNegativeNullParameter() {
+        Assert.assertFalse(FieldDataValidator.isValidUserEmail(null));
+    }    @Test
+    public void testIsValidUserEmailNegativeWrongEmail() {
+        String email = "email";
+        Assert.assertFalse(FieldDataValidator.isValidUserEmail(email));
+    }
+
+    @Test
+    public void testIsValidUserPasswordPositive() {
+        String pass = "passw#%:;=’@&$|asdfas addf";
+        Assert.assertTrue(FieldDataValidator.isValidUserPassword(pass));
+    }
+
+    @Test
+    public void testIsValidUserPasswordNegativeNullParameter() {
+        Assert.assertFalse(FieldDataValidator.validateRequestParameter(null));
+    }
+
+    @Test
+    public void testIsValidUserPasswordNegativeWrongParameter() {
+        String pass = "passw#%:;=’@&$|asdfas <>addf";
+        Assert.assertFalse(FieldDataValidator.validateRequestParameter(pass));
+    }
+
+    @Test
+    public void testIsValidUserPasswordNegativeWrongLength() {
+        String pass = "pass";
+        Assert.assertFalse(FieldDataValidator.isValidUserPassword(pass));
+    }
+
+    @Test
+    public void testValidateRequestParameterPositive() {
+        String param = "good";
+        Assert.assertTrue(FieldDataValidator.validateRequestParameter(param));
+    }
+
+    @Test
+    public void testValidateRequestParameterNegativeNullParameter() {
+        Assert.assertFalse(FieldDataValidator.validateRequestParameter(null));
+    }
+
+    @Test
+    public void testValidateRequestParameterNegativeWrongParameter1() {
+        String param = "wrong<>";
+        Assert.assertFalse(FieldDataValidator.validateRequestParameter(param));
+    }
+
+    @Test
+    public void testValidateRequestParameterNegativeWrongParameter2() {
+        String param = "pass{}";
+        Assert.assertFalse(FieldDataValidator.validateRequestParameter(param));
+    }
+
+    @Test
+    public void testValidateRequestTextAreaParameterPositive() {
+        String param = "good";
+        Assert.assertTrue(FieldDataValidator.validateRequestTextAreaParameter(param));
+    }
+
+    @Test
+    public void testValidateRequestTextAreaParameterNegativeNullParameter() {
+        Assert.assertFalse(FieldDataValidator.validateRequestTextAreaParameter(null));
+    }
+
+    @Test
+    public void testValidateRequestTextAreaParameterNegativeWrongParameter() {
+        String param = "wrong<>";
+        Assert.assertFalse(FieldDataValidator.validateRequestTextAreaParameter(param));
     }
 }
